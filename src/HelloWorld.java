222d1a3b2df4e0d5fb48c43d97f3ede0900fc0a8
Daniel.Laga <daniellaga84@gmail.com>
2020-08-14 17:57:40 +0200

    Initial Java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+        int passanger;
+        passanger = 0;
+        passanger = passanger + 12;
+        passanger = passanger - 3 + 5;
+        passanger = passanger + 2;
+        passanger = passanger + 14;
+        passanger = passanger - 10;
+        passanger = passanger + 5 - 4;
+        passanger = passanger - 1;
+        System.out.println(passanger);
+    }
+
+}
