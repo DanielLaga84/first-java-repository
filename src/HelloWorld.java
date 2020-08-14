4baffeb43f49713c69321062238cbf927e7f4950
Daniel.Laga <daniellaga84@gmail.com>
2020-08-14 19:58:11 +0200

    Added 3 tasks to this build and rebuild HelloWorld.jar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,16 +1,34 @@
-public class HelloWorld {
+public class HelloWorld<day> {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
-        int passanger;
-        passanger = 0;
-        passanger = passanger + 12;
-        passanger = passanger - 3 + 5;
-        passanger = passanger + 2;
-        passanger = passanger + 14;
-        passanger = passanger - 10;
-        passanger = passanger + 5 - 4;
-        passanger = passanger - 1;
-        System.out.println(passanger);
-    }
+        int passenger;
+        passenger = 0;
+        passenger = passenger + 12;
+        passenger = passenger - 3 + 5;
+        passenger = passenger + 2;
+        passenger = passenger + 14;
+        passenger = passenger - 10;
+        passenger = passenger + 5 - 4;
+        passenger = passenger - 1;
+        System.out.println(passenger);
 
+
+
+        double register = 10.0;
+        register = register + 5; //Customer pays $5.
+        register = register - 2.5; //Customer receives $2.50 as change.
+        register = register + 10; //Customer pays $10.
+        register = register - 3; //Customer receives $3 as change.
+        System.out.println(register);
+
+
+
+
+        int bankBalance;
+        bankBalance = 500;
+        bankBalance = bankBalance + 250;
+        bankBalance = bankBalance - 100;
+        System.out.println(bankBalance);
+
+}
 }
